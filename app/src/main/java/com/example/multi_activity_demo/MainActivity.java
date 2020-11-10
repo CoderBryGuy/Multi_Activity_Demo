c5a78f0db2e9341b9a8b50ac7666e6eb5ec2eb23
Bryan <bryan.dov.bergman@gmail.com>
2020-11-10 20:56:29 +0200

    done with this app

--- app/src/main/java/com/example/multi_activity_demo/MainActivity.java
+++ app/src/main/java/com/example/multi_activity_demo/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.multi_activity_demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView myListView;
+    ArrayAdapter<String> myAdapter;
+    String name;
+    List<String> myFriends;
+
     public void goToNext(View view){
         Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        intent.putExtra("name", name);
         startActivity(intent);
     }
 
@@ -16,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myListView = (ListView)findViewById(R.id.myListView);
+
+        myFriends = Arrays.asList("mike", "lori", "andrew", "yossi", "jason", "vicki", "nick", "aviel");
+
+        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFriends );
+
+        myListView.setAdapter(myAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+              name = myFriends.get(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/multi_activity_demo/SecondActivity.java
+++ app/src/main/java/com/example/multi_activity_demo/SecondActivity.java
@@ -3,18 +3,25 @@ package com.example.multi_activity_demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     public void goBack(View view){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+        finish();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+       Intent intent = getIntent();
+       String name = intent.getStringExtra("name");
+
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 }
